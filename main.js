55a8ec9f9cd160fcba4c04f10795f41d7bb264d2
Dylan TAILDEMAN <taildeman_dylan@outlook.com>
2022-06-09 16:41:01 +0200

    updated main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function getCalendar(){
-    let promise = new Promise(function(success, error){
-        let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange=function(){
-            if(this.readyState !== 4) return;
-            if(this.status == 200) return console.log(xhr.responseText)
-        }
-        xhr.open("GET", "reader.php", true);
-        xhr.send();
-    }.bind(this));
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange=function(){
+        if(this.readyState !== 4) return;
+        if(this.status == 200){
+            console.log(xhr.responseText)
+        } 
+    }
+    xhr.open("GET", "reader.php", true);
+    xhr.send();
 }
 
 window.onload = function(){
